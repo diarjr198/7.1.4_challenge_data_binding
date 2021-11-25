8f07ee7da9fbd1e100009e0671f1a5c04d9c69e7
diarjr <diar.student@gmail.com>
2021-11-25 19:46:54 +0700

    second challenge angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'first-time-angular';
+  number = 1;
+  serverName = '';
+
+  // change(): any {
+  //   this.number++;
+  // }
+
+  reset(): any {
+    this.serverName = '';
+    document.querySelector<HTMLInputElement>(`input`)?.focus();
+  }
 }
